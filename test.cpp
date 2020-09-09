67c381ad768b164f6ffe9923bb8da69cb5d8d44f
Kerrick <Kerrick>
2020-09-08 20:38:25 -0400

    example

--- test.cpp
+++ test.cpp
@@ -12,7 +12,7 @@ bool fake_function(int num)
     if (num != 0)
     {
         cout << "Working properly" << endl;
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
